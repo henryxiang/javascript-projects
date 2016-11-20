b55551a4f9e7bdecba185a3a6a50b7964f1c3a12
Henry Xiang <henryxiang@gmail.com>
2016-11-19 22:21:25 -0800

    added react-toolbox to react-component

--- react-component/webpack.config.js
+++ react-component/webpack.config.js
@@ -1,81 +1,57 @@
-var webpack = require('webpack');
-var ExtractTextPlugin = require("extract-text-webpack-plugin");
-var autoprefixer = require('autoprefixer');
-var path = require('path');
+const path = require('path');
+const webpack = require('webpack');
+const autoprefixer = require('autoprefixer');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-  entry: {
-    bundle: './src/main.jsx',
-    vendors: ['react', 'react-dom', 'mobx', 'mobx-react']
-  },
-  output: {
-    path: './dist',
-    filename: "[name].js"
-  },
-  devServer: {
-    inline: true,
-    port: 8000,
-    contentBase: './dist'
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader'
-      },
-      // {
-      //   test: /\.css$/,
-      //   // loader: "style-loader!css-loader"
-      //   loader: ExtractTextPlugin.extract("style-loader", "css-loader")
-      //   // loader: ExtractTextPlugin.extract("style-loader", "css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss-loader")
-      // },
-      {
-        test: /(\.scss|\.css)$/,
-        loader: ExtractTextPlugin.extract('style', 'css?sourceMap&modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss!sass')
-      },
-      {
-        test: /\.less$/,
-        loader: "style-loader!css-loader!less-loader"
-      },
-      // {
-      //   test: /\.scss$/,
-      //   //loader: "style-loader!css-loader!sass-loader"
-      //   loaders: ["style", "css", "sass"]
-      // },
-      {
-        test: /\.gif$/,
-        loader: "url-loader?mimetype=image/gif"
-      },
-      {
-        test: /\.png$/,
-        loader: "url-loader?mimetype=image/png"
-      },
-      {
-        test: /\.woff(2)?(\?v=[0-9].[0-9].[0-9])?$/,
-        loader: "url-loader?mimetype=application/font-woff"
-      },
-      {
-        test: /\.(ttf|eot|svg)(\?v=[0-9].[0-9].[0-9])?$/,
-        loader: "url-loader?mimetype=application/font-[ext]"
-      }
-    ]
-  },
-  plugins: [
-    new webpack.optimize.UglifyJsPlugin({minimize: true}),
-    new webpack.optimize.CommonsChunkPlugin('vendors', 'vendors.js'),
-    new ExtractTextPlugin("css/styles.css", {allChunks: true})
+  context: __dirname,
+  devtool: 'inline-source-map',
+  entry: [
+    'webpack-hot-middleware/client',
+    './src/app/client.js'
   ],
-  postcss: [autoprefixer],
-  // sassLoader: {
-  //   data: '@import "theme/_config.scss";',
-  //   includePaths: [path.resolve(__dirname, './src/app')]
-  // },
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
   resolve: {
-    extensions: ['', '.js', '.jsx', '.json', '.css', '.scss'],
+    extensions: ['', '.scss', '.css', '.js', '.json'],
     modulesDirectories: [
       'node_modules',
       path.resolve(__dirname, './node_modules')
     ]
-  }
-}
+  },
+  module: {
+    loaders: [
+      {
+        test: /(\.js|\.jsx)$/,
+        exclude: /(node_modules)/,
+        loader: 'babel',
+        query: { presets: ['es2015', 'stage-0', 'react'] }
+      }, {
+        test: /(\.scss|\.css)$/,
+        loader: ExtractTextPlugin.extract('style', 'css?sourceMap&modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss!sass')
+      }
+    ]
+  },
+  postcss: [autoprefixer],
+  sassLoader: {
+    data: '@import "theme/_config.scss";',
+    includePaths: [path.resolve(__dirname, './src/app')]
+  },
+  plugins: [
+    new ExtractTextPlugin('bundle.css', { allChunks: true }),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+      filename: 'vendor.bundle.js',
+      minChunks: Infinity
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin(),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('development')
+    })
+  ]
+};
