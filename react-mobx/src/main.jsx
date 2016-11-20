e239d9935ee967e3af15c8076fc2bd320fe2c163
Henry Xiang <henryxiang@gmail.com>
2016-11-20 11:41:09 -0800

    react-mobx code refactoring

--- react-mobx/src/main.jsx
+++ react-mobx/src/main.jsx
@@ -4,10 +4,11 @@ import 'font-awesome/css/font-awesome.min.css';
 import React from 'react'
 import ReactDOM from 'react-dom'
 import CounterListView from './view/counter-list'
-import CounterList from './model/counter-list'
+import Counter from './model/counter'
+import ObservableList from './model/observable-list'
 
-const counterList = new CounterList()
-counterList.addCounter()
+const counterList = new ObservableList()
+counterList.add(new Counter())
 
 ReactDOM.render(
   <CounterListView counterList={counterList} />,
--- react-mobx/src/view/counter-list.jsx
+++ react-mobx/src/view/counter-list.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import {observer} from 'mobx-react'
 import Button from 'react-bootstrap/lib/Button'
 import CounterView from './counter'
+import Counter from '../model/counter'
 
 const defaultStyles = {
     counterAddButton: {
@@ -19,21 +20,20 @@ const defaultStyles = {
       <div>
         <Button bsStyle="primary" bsSize="xs" style={styles.counterAddButton}
                 /* Action: counterList.addCounter() */
-                onClick={(event) => {counterList.addCounter()}}>
+                onClick={(event) => {counterList.add(new Counter())}}>
           <i className="fa fa-plus" />
         </Button>
 
         {
           /* Observable: counterList.counters */
-          counterList.counters.map(counter => {
-          /* Function that generates values derived from observables */
-          // counterList.getSortedCounters("desc").map(counter => {
+          counterList.items.map(counter => {
+          // this.getSortedCounters("desc").map(counter => {
             return (
               <div key={counter._id}>
                 <CounterView counter={counter} />
                 <Button bsStyle="danger" bsSize="xs"
                         /* Action: counterList.removeCounter() */
-                        onClick={(event) => {counterList.removeCounter(counter)}}>
+                        onClick={(event) => {counterList.remove(counter)}}>
                   <i className="fa fa-trash" />
                 </Button>
               </div>
@@ -43,12 +43,24 @@ const defaultStyles = {
 
         <div>
              {/* Computed properties: counterList.numberOfCounters and counterList.totalCount */}
-             <b>Counters: {counterList.numberOfCounters}</b>
+             <b>Counters: {counterList.size}</b>
              {" "}
-             <b>Total: {counterList.totalCount}</b>
+             <b>Total: {this.getTotalCount}</b>
         </div>
       </div>
     )
+  }
+
+  /* Function that generates values derived from observables */
+  getSortedCounters(order="desc") {
+    const counters = this.props.counterList.items
+    const m = order === "desc" ? -1 : 1
+    return counters.sort((a,b) => a.count >= b.count ? 1*m : -1*m)
+  }
+
+  getTotalCount() {
+    const counters = this.props.counterList.items
+    return this.counters.reduce((total, counter) => total + counter.count, 0)
   }
 }
 
