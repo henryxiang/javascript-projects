c5b5dbd038d7ae6391cb979a138598a792485b65
Henry Xiang <hxxiang@ucdavis.edu>
2017-06-14 18:11:59 -0700

    added test suite for counter view

--- webpack2-react-mobx-antd/src/view/counter.test.js
+++ webpack2-react-mobx-antd/src/view/counter.test.js
@@ -7,11 +7,33 @@ import renderer from 'react-test-renderer';
 
 describe('Counter View component', () => {
 
+  const preventDefault = () => {}
+
   it('renders Counter component', () => {
     const wrapper = shallow(
         <CounterView counter={new Counter()}/>
     );
-    // console.log(wrapper);
+    // console.log(wrapper.find('root'));
     expect(wrapper).toMatchSnapshot();
   })
+
+  it('should have initial 0 count', () => {
+    const counter = new Counter()
+    const wrapper = shallow(
+        <CounterView counter={counter} />
+    );
+    expect(wrapper.find('Badge').html()).toMatch(/<span.+>0<\/span>/)    
+  })
+
+  it('should have count of 1 after one click', () => {
+    const counter = new Counter()
+    const wrapper = shallow(
+        <CounterView counter={counter} />
+    );
+    expect(wrapper.find('Badge').html()).toMatch(/<span.+>0<\/span>/)
+    wrapper.find('Badge').simulate('click', {preventDefault})
+    wrapper.find('Badge').simulate('click', {preventDefault})
+    expect(wrapper.find('Badge').html()).toMatch(/<span.+>2<\/span>/) 
+  })
+
 });
