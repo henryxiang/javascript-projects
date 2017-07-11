150470f33cbc4e63fb727fd9b655453a5d4b84a8
Henry Xiang <hxxiang@ucdavis.edu>
2017-07-10 17:30:40 -0700

    created simple demo app

---
+++ express-passport/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+
+const port = 3000;
+const context = '/demo';
+const documentRoot = path.join(__dirname, 'public');
+
+
+const app = express();
+
+app.use(context, express.static(documentRoot));
+
+app.listen(port, () => {
+  console.log(`Server URL: http://localhost:${port}${context}`);
+});
