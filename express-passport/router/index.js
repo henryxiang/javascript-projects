446c1710c3811843e3a58a562fe8a3eaf4a35740
Henry Xiang <henryxiang@gmail.com>
2017-07-11 05:41:14 -0700

    reconfigured authentication middleware

--- express-passport/router/index.js
+++ express-passport/router/index.js
@@ -1,14 +1,20 @@
-const homeRoute = require('./home-route');
-const logoutRoute = require('./logout-route');
 const auth = require('../auth');
+const homeRoute = require('./home-route');
+const userRoute = require('./user-route');
+const logoutRoute = require('./logout-route');
+
 const context = '/demo'
 
 module.exports = (app, passport) => {
   auth.configure(passport);
+
   app.use((req, res, next) => {
     res.set('Cache-Control', 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0');
     next();
   });
-  app.use(`${context}/`, auth.authenticate(passport), homeRoute);
-  app.use(`${context}/logout`, logoutRoute);
+  app.use(auth.authenticate(passport));
+  
+  app.use(`${context}/`, homeRoute); 
+  app.use(`${context}/user`, userRoute); 
+  app.use(`${context}/logout`, logoutRoute);  
 };
---
+++ express-passport/router/user-route.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+
+router.get('/', (req, res) => {
+  res
+    .status(200)
+    .send(JSON.stringify(req.user));
+});
+
+module.exports = router;
