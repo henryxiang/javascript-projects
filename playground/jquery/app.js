dcad6ebaf2d7eac39a0ac6d004b7869743d5c42c
Henry Xiang <henryxiang@gmail.com>
2018-09-13 11:07:36 -0700

    created playground/jquery

--- playground/jquery/app.js
+++ playground/jquery/app.js
@@ -1,1 +1,14 @@
-$('#app').append('Hello World!');
+$(() => {
+  const info = $('<div>').text('Hello, World!');
+  const loc = $('<div>');
+  const btn = $('<button>').text('Click Me');
+
+  $('#app').append(info, loc, btn);
+
+  btn.click(() => info.text('You clicked me.').show());
+
+  info.click(() => info.hide());
+
+  $('body').mousemove((e) => loc.text(`${e.clientX}, ${e.clientY}`));
+})
+
