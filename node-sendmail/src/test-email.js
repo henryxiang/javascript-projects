d6890d230222f6b9a532a4100a01d5743a0bba88
Henry Xiang <henryxiang@gmail.com>
2019-08-07 11:12:23 -0700

    updated node-sendmail project

--- node-sendmail/src/test-email.js
+++ node-sendmail/src/test-email.js
@@ -2,7 +2,8 @@ const sendmail = require('sendmail')({ silent: false });
  
 sendmail({
     from: 'AFS JIRA<afs-jira@ucdavis.edu>',
-    to: 'hxxiang@ucdavis.edu',
+    to: 'henryxiang@gmail.com',
+    //to: 'hxxiang@ucdavis.edu',
     subject: 'Test Sendmail',
     //html: 'Mail of test from Fiscal Cert Admin.',
     text: 'Mail of test from Fiscal Cert Admin.',
